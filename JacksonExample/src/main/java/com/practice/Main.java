3c418cfd2fc89f866b233710ecb0383923366ef8
ObradovicZ <obradtf@gmail.com>
2018-01-15 17:03:54 +0100

    added jackson 2.8.10 dependency to pom.xml

--- JacksonExample/src/main/java/com/practice/Main.java
+++ JacksonExample/src/main/java/com/practice/Main.java
@@ -1,9 +1,11 @@
 package com.practice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Main {
     public static void main(String[] args) {
-	
+	ObjectMapper objectMapper = new ObjectMapper();
     }
 
 }
