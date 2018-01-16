2d11263987496e69580494305168028231764243
ObradovicZ <obradtf@gmail.com>
2018-01-16 15:01:19 +0100

    Java object to json implemented

--- JacksonExample/src/main/java/com/practice/Main.java
+++ JacksonExample/src/main/java/com/practice/Main.java
@@ -1,11 +1,49 @@
 package com.practice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.practice.Entity.Album;
+import com.practice.Entity.Artist;
+import com.practice.Mapper.JsonObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
 
 
 public class Main {
     public static void main(String[] args) {
-	ObjectMapper objectMapper = new ObjectMapper();
+	
+//	All needed objects
+	ObjectMapper om = new ObjectMapper();
+	Album album  = new Album("Parsing the JSON!");
+	LocalDate ld = LocalDate.of(2009, Month.MAY, 9);
+	Artist artist = new Artist();
+	
+//	Setting artist fields
+	artist.name="Jackson";
+	artist.birthDate=ld.toString();
+	
+//	Setting album fields
+	album.setArtist(artist);
+	album.setSongs(new ArrayList<String>(){{
+		   add("Just parse it");
+		   add("Go Json");
+		   add("Json Json");
+	}});
+	
+//	Writting json
+	JsonObjectMapper.writeJson(album, om, "album.json");
+	
+
+
+	
+	
+	
+	
+	
     }
 
 }
